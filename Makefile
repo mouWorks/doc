62f041bf6dcc48e88517a08a22237f43c66f613f
Mou <mou@zuvio.com>
2019-08-28 11:00:18 +0800

    Trying

---
+++ Makefile
@@ -0,0 +1,14 @@
+#!/usr/bin/make -f
+
+start:
+	@echo ">>> Bring up Local Docs"
+	vuepress dev docs
+
+pull:
+	@echo ">>> Getting the latest code"
+	git pull origin master --rebase
+
+push: |
+	@echo ">>> Pushed to master and update github Pages"
+	git push origin master
+	npm deploy
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'MouWorks blog',
+  description: 'MouWorks',
+  head: [ // 注入到当前页面的 HTML <head> 中的标签
+    ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'manifest', href: '/photo.jpg' }],
+    ['link', { rel: 'apple-touch-icon', href: '/photo.jpg' }],
+  ],
+  serviceWorker: true, // 是否开启 PWA
+  base: '/', // 这是部署到github相关的配置
+  markdown: {
+    lineNumbers: false // 代码块显示行号
+  },
+  themeConfig: {
+    nav:[ // 导航栏配置
+      {text: '學習資源', link: '/learning/' },
+      {text: 'LeetCode', link: '/leetcode/'},
+      {text: 'Site', link: 'https://www.m0u.work'}      
+    ],
+    sidebar: 'auto', // 侧边栏配置
+    sidebarDepth: 2, // 侧边栏显示2级
+  }
+};
